5d1448d9715880a4253c4d80b73d94ee25329f4c
P̤radeep <78857587+Pradeep-iKp@users.noreply.github.com>
2023-01-10 13:44:05 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -15,7 +15,7 @@ from config import ADMINS, FORCE_MSG, START_MSG, CUSTOM_CAPTION, DISABLE_CHANNEL
 from helper_func import subscribed, encode, decode, get_messages
 from database.database import add_user, del_user, full_userbase, present_user
 
-TEXT = """ Hello """
+TEXT = """ Under Development! """
 
 
 @Bot.on_message(filters.command('start') & filters.private & subscribed)
