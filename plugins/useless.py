ae028da0e9e0967adbfb23b36807da3ed55cb8a2
P̤radeep <78857587+Pradeep-iKp@users.noreply.github.com>
2023-02-22 22:13:18 +0530

    Update useless.py

--- plugins/useless.py
+++ plugins/useless.py
@@ -1,7 +1,7 @@
 from bot import Bot
 from pyrogram.types import Message
 from pyrogram import filters
-from config import ADMINS, BOT_STATS_TEXT, USER_REPLY_TEXT
+from config import ADMINS, BOT_STATS_TEXT
 from datetime import datetime
 from helper_func import get_readable_time
 
@@ -13,7 +13,4 @@ async def stats(bot: Bot, message: Message):
     await message.reply(BOT_STATS_TEXT.format(uptime=time))
 
 
-@Bot.on_message(filters.private)
-async def useless(_,message: Message):
-    if USER_REPLY_TEXT:
-        await message.reply(USER_REPLY_TEXT)
+
